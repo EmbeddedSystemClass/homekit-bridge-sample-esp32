0215985da3fd3f31935c2491883003da361c2719
ouyang <ouyangchengfeng@xiaomi.com>
2019-04-28 17:44:14 +0800

    add flash.sh

---
+++ flash.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export ESP_IDF=/opt/esp-idf
+export IDF_PATH=$ESP_IDF
+export ESP32_TOOLCHAIN=/opt/xtensa-esp32-elf
+export PATH=$ESP32_TOOLCHAIN/bin:$PATH
+sh $ESP_IDF/add_path.sh
+
+cd build
+make flash
